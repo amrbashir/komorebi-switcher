17873c657e2bda141875424ece742adfdacd7c5d
amrbashir <github@amrbashir.me>
2026-01-03 18:03:18 +0200

    feat: add hombrew instructions

--- .scripts/generate-release-notes.sh
+++ .scripts/generate-release-notes.sh
@@ -6,19 +6,6 @@ out="RELEASE_NOTES.md"
 is_latest_release=false
 is_in_latest_release=false
 
-# Clear output file
-> "$out"
-
-# Add macOS quarantine notice
-cat << EOF >> "$out"
-> [!TIP]
->
-> on macOS, downloaded apps are quarantined by default. To fix this, run:
-> \`\`\`bash
-> xattr -d com.apple.quarantine /Applications/komorebi-switcher.app
-> \`\`\`
-EOF
-
 # Extract latest release notes
 while IFS= read -r line; do
     # Skip [Unreleased] section
@@ -38,3 +25,13 @@ while IFS= read -r line; do
         echo "$line" >> "$out"
     fi
 done < "$path"
+
+# Add macOS quarantine notice
+cat << EOF >> "$out"
+> [!TIP]
+>
+> on macOS, downloaded apps are quarantined by default. To fix this, run:
+> \`\`\`bash
+> xattr -d com.apple.quarantine /Applications/komorebi-switcher.app
+> \`\`\`
+EOF
