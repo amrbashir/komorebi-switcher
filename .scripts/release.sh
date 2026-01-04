961567f21a4bca059612cceeedf4b5a315638efa
amrbashir <github@amrbashir.me>
2026-01-04 02:39:32 +0200

    chore: use gnu-sed syntax in release.sh

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -3,19 +3,19 @@
 version="$1"
 
 path="Cargo.toml"
-sed -i '' -E "s/version = \"[0-9]+\.[0-9]+\.[0-9]+\"/version = \"$version\"/" "$path"
+sed -i -E "s/version = \"[0-9]+\.[0-9]+\.[0-9]+\"/version = \"$version\"/" "$path"
 
 cargo update -p komorebi-switcher # update the lock file
 
 path="installer/installer.nsi"
-sed -i '' -E "s/VERSION \"[0-9]+\.[0-9]+\.[0-9]+\"/VERSION \"$version\"/" "$path"
+sed -i -E "s/VERSION \"[0-9]+\.[0-9]+\.[0-9]+\"/VERSION \"$version\"/" "$path"
 
 path="installer/Info.plist"
-sed -i '' -E "s/<string>[0-9]+\.[0-9]+\.[0-9]+<\/string>/<string>$version<\/string>/" "$path"
+sed -i -E "s/<string>[0-9]+\.[0-9]+\.[0-9]+<\/string>/<string>$version<\/string>/" "$path"
 
 path="CHANGELOG.md"
 date=$(date +%Y-%m-%d)
-sed -i '' "s/## \[Unreleased\]/## [Unreleased]\n\n## [$version] - $date/" "$path"
+sed -i "s/## \[Unreleased\]/## [Unreleased]\n\n## [$version] - $date/" "$path"
 
 git add .
 git commit -m "release: v$version"
